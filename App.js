16368a7056ba191cb47d301ef9493727690dd3d2
Uday Shankar Paidimarri <udayshankarpaidimarri@Udays-MacBook-Air.local>
2024-04-04 18:41:00 -0400

    secod episode

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", { id: "child" }, "hello world from react H1 !"),
   React.createElement("h2", { id: "child2" }, "hello world from react H2!"),
